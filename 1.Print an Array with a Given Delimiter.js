a752c91e18195ea6e2f93937db5ab3f5497e511f
Miroslav Tsarev <mtzarev@gmail.com>
2024-04-13 21:37:46 +0300

    Add files via upload

---
+++ 1.Print an Array with a Given Delimiter.js
@@ -0,0 +1,20 @@
+function printAnArrayWithAGivenDelimiter(arr, str) {
+
+console.log(arr.join(`${str}`));
+  
+}
+printAnArrayWithAGivenDelimiter(['One', 
+'Two', 
+'Three', 
+'Four', 
+'Five'], 
+'-'
+);
+// printAnArrayWithAGivenDelimiter(['How about no?', 
+// 'I',
+// 'will', 
+// 'not', 
+// 'do', 
+// 'it!'], 
+// '_'
+// );
